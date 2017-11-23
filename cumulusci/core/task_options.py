90ed8cff963dc77bab47a3215ae7fa3b9bb7a776
Christian Carter <christian.carter@salesforce.com>
2017-11-23 10:54:54 -0800

    factor out task option handlers

---
+++ cumulusci/core/task_options.py
@@ -0,0 +1,19 @@
+""" Mixins for handling task options """
+
+from cumulusci.core.exceptions import TaskOptionsError
+
+class CCIOptionHandlerMixin(object):
+    def _validate_options(self):
+        missing_required = []
+        for name, config in list(self.get_task_options().items()):
+            if config.get('required') is True and name not in self.options:
+                missing_required.append(name)
+
+        if missing_required:
+            raise TaskOptionsError(
+                '{} requires the options ({}) '
+                'and no values were provided'.format(
+                    self.__class__.__name__,
+                    ', '.join(missing_required),
+                )
+            )
--- cumulusci/core/tasks.py
+++ cumulusci/core/tasks.py
@@ -78,21 +78,6 @@ class Task(object):
         task_options = self.task_options
         return task_options
 
-    def _validate_options(self):
-        missing_required = []
-        for name, config in list(self.get_task_options().items()):
-            if config.get('required') is True and name not in self.options:
-                missing_required.append(name)
-
-        if missing_required:
-            raise TaskOptionsError(
-                '{} requires the options ({}) '
-                'and no values were provided'.format(
-                    self.__class__.__name__,
-                    ', '.join(missing_required),
-                )
-            )
-
     def __call__(self):
         try:
             self._log_begin()
@@ -111,6 +96,9 @@ class Task(object):
 
     def _init_task(self):
         """ Override to implement dynamic logic for initializing the task. """
+        pass
+
+    def _validate_options(self):
         pass
 
     def _process_exception(self, e):
